12e44f0f1ef327132dfec6293e513b26769eef2e
CarlosPulidoHernandez <carlos.pulido@alu.uclm.es>
2023-09-21 19:44:26 +0200

    test

--- src/main/java/uclm/esi/filemanagement/GenericFile.java
+++ src/main/java/uclm/esi/filemanagement/GenericFile.java
@@ -1,5 +1,53 @@
 package uclm.esi.filemanagement;
 
 public class GenericFile {
+	
+	public static void main(String [] args){
+			
+		int x = 10;
+
+        if (x == 1) {
+            System.out.println("x is 1");
+        } else if (x == 2) {
+            System.out.println("x is 2");
+        } else if (x == 3) {
+            System.out.println("x is 3");
+        } else if (x == 4) {
+            System.out.println("x is 4");
+        } else if (x == 5) {
+            System.out.println("x is 5");
+        } else if (x == 6) {
+            System.out.println("x is 6");
+        } else if (x == 7) {
+            System.out.println("x is 7");
+        } else if (x == 8) {
+            System.out.println("x is 8");
+        } else if (x == 9) {
+            System.out.println("x is 9");
+        } else if (x == 10) {
+            System.out.println("x is 10");
+        } else if (x == 11) {
+            System.out.println("x is 11");
+        } else if (x == 12) {
+            System.out.println("x is 12");
+        } else if (x == 13) {
+            System.out.println("x is 13");
+        } else if (x == 14) {
+            System.out.println("x is 14");
+        } else if (x == 15) {
+            System.out.println("x is 15");
+        } else if (x == 16) {
+            System.out.println("x is 16");
+        } else if (x == 17) {
+            System.out.println("x is 17");
+        } else if (x == 18) {
+            System.out.println("x is 18");
+        } else if (x == 19) {
+            System.out.println("x is 19");
+        } else if (x == 20) {
+            System.out.println("x is 20");
+        }
+		
+	}
 
 }
